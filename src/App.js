38a3703b9e75fd831bc7f094263317e330f1b0d2
Cory <corys4139@gmail.com>
2021-08-22 22:12:20 -0400

    adjust ARIA roles

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import List from './components/List';
-import Navbar from './components/Navbar';
-import Form from './components/Form'
-import BookContextProvider from './contexts/BookContext';
+import List from "./components/List";
+import Navbar from "./components/Navbar";
+import Form from "./components/Form";
+import BookContextProvider from "./contexts/BookContext";
 
 function App() {
-  return (
-    <div className="App">
-      <BookContextProvider>
-        <div role="presentation/none" className="container">
-        <Navbar />
-        <List />
-        <Form />
-        </div>
-      </BookContextProvider>
-    </div>
-  );
+	return (
+		<div className="App">
+			<BookContextProvider>
+				<div role="presentation" className="container">
+					<Navbar />
+					<List />
+					<Form />
+				</div>
+			</BookContextProvider>
+		</div>
+	);
 }
 
 export default App;
